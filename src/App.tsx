391cad3c5ba05bdd1a0e3234069b661c3b7869d9
Gustavo Maia <ms.gustavo@outlook.com>
2024-01-19 10:29:58 -0300

    SearchBar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./styles/main.scss";
+import SearchBar from "./components/SearchBar/SearchBar";
 
-function App() {
+type Note = {
+  id: number;
+  title: string;
+  isFavorite: boolean;
+};
+
+const NoteApp: React.FC = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [newNoteTitle, setNewNoteTitle] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //TODO: Function to add a new note
+  const addNote = (title: string) => {};
+
+  //TODO: Function to toggle the favorite status of a note
+  const toggleFavorite = (id: number) => {};
+
+  //TODO: Function to handle note title input changes
+  const handleNoteTitleChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewNoteTitle(event.target.value);
+  };
+
+  //TODO: Function to handle search term changes
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <h1>CorelabChallenge</h1>
+    <div className="note-app">
+      <SearchBar
+        searchTerm={searchTerm}
+        handleSearchTermChange={handleSearchTermChange}
+      />
+
+      <div className="note-creation">
+        <input
+          type="text"
+          placeholder="Título"
+          value={newNoteTitle}
+          onChange={handleNoteTitleChange}
+        />
+        <button onClick={() => addNote(newNoteTitle)}>Criar nota</button>
+      </div>
+
+      {/* Notes List */}
+      {/* Favorites Section */}
+      {/* Note Upload Area */}
     </div>
   );
-}
+};
 
-export default App;
+export default NoteApp;
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./SearchBar.scss";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search.svg";
+
+type SearchBarProps = {
+  searchTerm: string;
+  handleSearchTermChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  searchTerm,
+  handleSearchTermChange,
+}) => {
+  return (
+    <div className="search-bar-container">
+      <img src={logo} alt="Logo" className="search-bar-logo" />
+      <span className="search-bar-text">CoreNotes</span>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Pesquisar notas"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          className="search-bar-input"
+        />
+        <button className="search-bar-button">
+          <img src={searchIcon} alt="SearchIcon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
